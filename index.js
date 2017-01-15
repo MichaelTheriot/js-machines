74024230f5aff70b44de10bca16ca383b858ebcc
Michael Theriot <michael.lee.theriot@gmail.com>
2017-01-15 17:58:24 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 exports.State = require('./state');
 exports.StateSet = require('./stateset');
 exports.NFAState = require('./nfastate');
-exports.EndState = require('./endstate');
-exports.failState = require('./failstate');
-exports.empty = require('./empty');
